cfeca5e927b3fabb7515ebe4e567eaebfbe7c865
Wendell Santos <wendellcrifort@gmail.com>
2021-03-02 17:24:33 -0300

    escalando o worker

--- worker/worker.py
+++ worker/worker.py
@@ -5,7 +5,7 @@ from random import randint
 
 if __name__ == '__main__':
     r = redis.Redis(host='queue', port=6379, db=0)
-
+    print('aguardando mensagens...')
     while True:
         mensagem = json.loads(r.blpop('sender')[1])
         print('mandando a mensagem', mensagem['assunto'])
